ac86157b3e7870b50cda7657805f687b2a53b5df
Artemiy <artemiusxyz@gmail.com>
2025-01-08 11:22:56 +0100

    Resolved the bug with occupancy field, now the simulation is dynamical, however doesn't yet seem to work correctly

--- eulerian_fluid_simulator.jl
+++ eulerian_fluid_simulator.jl
@@ -1,4 +1,4 @@
-using GLMakie
+using GLMakie, Observables
 
 mutable struct Fluid
     ρ::Float64 # density
@@ -10,7 +10,7 @@ mutable struct Fluid
     v::Array{Float64, 2} # velocity field by y
     vn::Array{Float64, 2} # velocity field by y new
     p::Array{Float64, 2} # static pressure field
-    o::Array{Float64, 2} # solid occupancy field
+    o::Array{Float64, 2} # fluid occupancy field
 
     function Fluid(ρ::Float64, dl::Float64, nx::Int64, ny::Int64)
         u  = zeros(Float64, nx, ny)
@@ -18,14 +18,20 @@ mutable struct Fluid
         v  = zeros(Float64, nx, ny)
         vn = zeros(Float64, nx, ny)
         p  = zeros(Float64, nx, ny)
-        o  = zeros(Float64, nx, ny)
+        o  = ones(Float64, nx, ny)
+        for i in 1:nx, j in 1:ny # defining border
+            i == 1  && (o[j,i] = 0.0)
+            i == nx && (o[j,i] = 0.0)
+            j == 1  && (o[j,i] = 0.0)
+            j == ny && (o[j,i] = 0.0)
+        end
         return new(ρ, dl, nx, ny, u, un, v, vn, p, o)
     end
 end
 
 function solveIncompressibility(fluid::Fluid, numIters::Int64, dt::Float64)
      n = fluid.ny
-     cp = fluid.ρ * fluid.dl / dt 
+     cp = fluid.ρ * fluid.dl / dt
      nx = fluid.nx
      ny = fluid.ny
 
@@ -35,7 +41,7 @@ function solveIncompressibility(fluid::Fluid, numIters::Int64, dt::Float64)
             ox0 = fluid.o[j,i-1]
             ox1 = fluid.o[j,i+1]
             oy0 = fluid.o[j-1,i]
-            ox1 = fluid.o[j+1,i]
+            oy1 = fluid.o[j+1,i]
             o   = ox0 + ox1 + oy0 + oy1
             o == 0.0 && continue
 
@@ -54,11 +60,11 @@ end
 
 function extrapolate(fluid::Fluid)
    for i in 1:fluid.nx
-       fluid.u[0,i] = fluid.u[1,i]
+       fluid.u[1,i] = fluid.u[2,i]
        fluid.u[fluid.ny,i] = fluid.u[fluid.ny-1,i]
    end 
    for j in 1:fluid.ny
-       fluid.v[j,0] = fluid.v[j,1]
+       fluid.v[j,1] = fluid.v[j,2]
        fluid.v[j,fluid.nx] = fluid.v[j,fluid.ny-1]
    end
 end
@@ -74,17 +80,17 @@ function sample_field(fluid::Fluid, x::Float64, y::Float64, field::String)
     dx = 0.0
     dy = 0.0
 
-    f
+    f = similar(fluid.u)
     field == "U_FIELD" ? (f = fluid.u; dy = h₂) : 
     field == "V_FIELD" ? (f = fluid.v; dx = h₂) : nothing
 
-    x₀ = min(floor((x - dx) * h₁), fluid.nx)
+    x₀ = Int(min(floor((x - dx) * h₁), fluid.nx))
     tx = ((x - dx) - x₀ * dl) * h₁
-    x₁ = min(x₀ + 1, fluid.nx)
+    x₁ = Int(min(x₀ + 1, fluid.nx))
 
-    y₀ = min(floor((y - dy) * h₁), fluid.ny)
-    tx = ((y - dy) - y₀ * dl) * h₁
-    y₁ = min(y₀ + 1, fluid.ny)
+    y₀ = Int(min(floor((y - dy) * h₁), fluid.ny))
+    ty = ((y - dy) - y₀ * dl) * h₁
+    y₁ = Int(min(y₀ + 1, fluid.ny))
 
     sx = 1.0 - tx
     sy = 1.0 - ty
@@ -106,8 +112,11 @@ end
 end
 
 function advect_velocity(fluid::Fluid, dt::Float64)
-    fluid.un, fluid.u = fluid.u, fluid.un
-    fluid.vn, fluid.v = fluid.v, fluid.vn
+#    fluid.un, fluid.u = fluid.u, fluid.un
+#    fluid.vn, fluid.v = fluid.v, fluid.vn
+    temp     = copy(fluid.un)
+    fluid.un = copy(fluid.u)
+    fluid.u  = copy(temp)
 
     dl = fluid.dl
     h₂ = 0.5 * dl 
@@ -131,13 +140,16 @@ function advect_velocity(fluid::Fluid, dt::Float64)
             x = x - dt * u
             y = y - dt * v
             v = sample_field(fluid,x,y,"V_FIELD")
-            fluid.uv[j,i] = v
+            fluid.un[j,i] = v
         end
     end
 
-    fluid.u, fluid.un = fluid.un, fluid.u
-    fluid.v, fluid.vn = fluid.vn, fluid.v
+   #fluid.u, fluid.un = fluid.un, fluid.u
+   #fluid.v, fluid.vn = fluid.vn, fluid.v
 
+    temp     = copy(fluid.u)
+    fluid.u  = copy(fluid.un)
+    fluid.un = copy(temp)
 end
 
 function simulate(fluid::Fluid, dt::Float64, numIters::Int64)
@@ -146,3 +158,52 @@ function simulate(fluid::Fluid, dt::Float64, numIters::Int64)
     extrapolate(fluid)
     advect_velocity(fluid, dt)
 end
+
+function to_pixels(value_in_cm::Float64)
+   dpi = sqrt(1920^2 + 1080^2) / 15.6 # should be fixed with each new monitor
+   return (value_in_cm / 2.54) * dpi
+end
+
+window_width    = 25.0 # (cm)
+window_height   = 25.0 # (cm)
+axis_width      = 20.0
+axis_height     = 20.0
+ρ = 1000.0; dl = 0.001; nx = 100; ny = 100
+
+fluid = Fluid(ρ, dl, nx, ny)
+ 
+set_theme!(theme_dark())
+fig = Figure(size = (to_pixels(window_width), to_pixels(window_height)))
+ax  = Axis(fig[1,1][1,1], 
+            title = "Pressure field", 
+            aspect = 1) 
+            #width = to_pixels(axis_width), 
+            #height = to_pixels(axis_height))
+            #perspectiveness = 1.0)
+data = Observable(similar(fluid.p))
+#volume!(ax, data, colorrange = (0.0, 10.0), algorithm = :absorption, absorption = 0.1)
+heatmap!(ax, data)
+display(fig)
+
+# Configuring UI
+quit = Observable(false)
+on(events(fig).keyboardbutton) do event
+   if event.action == Keyboard.press && event.key == Keyboard.escape # ESC key
+      quit[] = true
+   end
+end
+
+fps = 60
+fluid.u[Int(nx/2),Int(ny/2)] = 1.0
+
+println("The simulation is running...")
+while !quit[]
+    # run simulation and update figure data
+    simulate(fluid, 0.01, 5)
+    data[] = copy(fluid.p)
+    notify(data)
+    sleep(1/fps)
+    display(fig)
+end
+println("The simulation stopped...")
+GLMakie.closeall()
